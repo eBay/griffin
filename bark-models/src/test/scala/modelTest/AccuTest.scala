cabd5ee950bc995ae136f3f50cafa94d56de1b67
Liu <lliu13@corp.ebay.com>
2016-08-10 18:00:57 +0800

    update model test code

--- bark-models/src/test/scala/modelTest/AccuTest.scala
+++ bark-models/src/test/scala/modelTest/AccuTest.scala
@@ -1,109 +1,83 @@
 package modelTest
 
 import org.apache.bark._
-import org.apache.bark.util._
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.StructType
 import org.apache.spark.{SparkConf, SparkContext}
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
 
-import scala.collection.mutable.ArrayBuffer
-import org.scalatest.Assertions
-import org.scalatest.Matchers
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, Row, SQLContext}
-import java.io.{File, FileInputStream, FileOutputStream}
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import java.io.{FileInputStream, FileOutputStream}
+import scala.collection.mutable.MutableList
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
-class AccuTest extends FunSuite with Matchers {
+class AccuTest extends FunSuite with Matchers with BeforeAndAfter {
 
-  val dataFilePath = convertPath("data/test/dataFile/")
-  val reqJsonPath = convertPath("data/test/reqJson/")
-  val recordFilePath = convertPath("data/test/recordFile/")
+  val dataFilePath = FileLoaderUtil.convertPath("data/test/dataFile/")
+  val reqJsonPath = FileLoaderUtil.convertPath("data/test/reqJson/")
+  val recordFilePath = FileLoaderUtil.convertPath("data/test/recordFile/")
   val recordFileName = "_RESULT_ACCU"
-  val dataDescFileSuffix = "_type"
 
-  def convertPath(path: String) : String = {
-    path.replace("/", System.getProperty("file.separator"))
+  case class AccuData() {
+    var cnt: Int = _
+    var reqJson: String = _
+    var configure: AccuracyConfEntity = _
+    var dataFrameSrc: DataFrame = _
+    var dataFrameTgt: DataFrame = _
+    var result: ((Long, Long), List[String]) = _
   }
+  val accuDatas = MutableList[AccuData]()
 
-  def getAccTestReq() : ArrayBuffer[String] = {
-    val jsonFiles = ArrayBuffer[String](reqJsonPath + "accTest1.json", reqJsonPath + "accTest2.json", reqJsonPath + "accTest3.json")
-    jsonFiles
-  }
+  var sc: SparkContext = _
 
-  def loadDataFile(sqlContext: SQLContext, file: String, fmt: String = "com.databricks.spark.csv") = {
-    val reader = sqlContext.read
-      .format(fmt)
-      .option("header", "true")
-      .option("treatEmptyValuesAsNulls", "true")
-      .option("delimiter", ",")
-
-    val typeFile = file + dataDescFileSuffix
-    val typeExist = new File(typeFile).exists()
-    val schemaReader = if (typeExist) {
-      val types = reader.load(typeFile).collect.map( r => (r.getString(0), r.getString(1)) )
-      val fields = types.map(kt => StructField(kt._1, DataTypeUtils.str2DataType(kt._2)))
-      val customSchema = StructType(fields)
-      reader.schema(customSchema)
-    } else {
-      reader.option("inferSchema", "true")
-    }
-
-    schemaReader.load(file)
-  }
-
-  test("test accuracy file with srcFile and tgtFile") {
-    val reqFiles = getAccTestReq()
-
+  before {
     val conf = new SparkConf().setMaster("local[*]").setAppName("AccTest")
-    val sc = new SparkContext(conf)
-    if (sc.version.charAt(0) - '0' < 2) {
-      val sqlContext = new SQLContext(sc)
+    sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
 
-      val mapper = new ObjectMapper()
-      mapper.registerModule(DefaultScalaModule)
-
-      var cnt = 1;
-
-      val out = new FileOutputStream(recordFilePath + recordFileName)
-
-      reqFiles.foreach { reqFile =>
-        val input = new FileInputStream(reqFile)
-        val configure = mapper.readValue(input, classOf[AccuracyConfEntity])
-
-        //source DataFrame
-        val srcFile = dataFilePath + configure.source
-        val srcdf = loadDataFile(sqlContext, srcFile)
-
-        //target DataFrame
-        val tgtFile = dataFilePath + configure.target
-        val tgtdf = loadDataFile(sqlContext, tgtFile)
-
-        //-- algorithm --
-        val ((missCount, srcCount), missedList) = Accu.calcAccu(sc, configure, srcdf, tgtdf)
-
-        //output
-        out.write(("//" + "=" * 10).getBytes("utf-8"))
-        out.write((s" $cnt.. Test Accuracy model result with request file: $reqFile ").getBytes("utf-8"))
-        out.write(("=" * 10 + "\n").getBytes("utf-8"))
-
-        val rslt = s"match percentage: ${((1 - missCount.toDouble / srcCount) * 100)} %"
-        val rcds = missedList.mkString("\n")
-        val rcd = rslt + "\n\n" + rcds + "\n\n";
-
-        out.write(rcd.getBytes("utf-8"))
-
-        cnt += 1
-      }
-
-      out.close()
-
-      sc.stop()
+    var cnt = 1;
+    val accTests = List("accTest1.json", "accTest2.json", "accTest3.json")
+    for (tf <- accTests) {
+      val reqJson = reqJsonPath + tf
+      val accuData = new AccuData()
+      accuData.cnt = cnt
+      accuData.reqJson = reqJson
+      val input = new FileInputStream(reqJson)
+      accuData.configure = mapper.readValue(input, classOf[AccuracyConfEntity])
+      accuData.dataFrameSrc = FileLoaderUtil.loadDataFile(sqlContext, dataFilePath + accuData.configure.source)
+      accuData.dataFrameTgt = FileLoaderUtil.loadDataFile(sqlContext, dataFilePath + accuData.configure.target)
+      accuDatas += accuData
+      cnt += 1
     }
+  }
+
+  test("test accuracy requests") {
+    for (accuData <- accuDatas) {
+      //-- algorithm --
+      accuData.result = Accu.calcAccu(accuData.configure, accuData.dataFrameSrc, accuData.dataFrameTgt)
+    }
+  }
+
+  after {
+    val out = new FileOutputStream(recordFilePath + recordFileName)
+    for (accuData <- accuDatas) {
+      //output
+      out.write(("//" + "=" * 10).getBytes("utf-8"))
+      out.write((s" ${accuData.cnt}. Test Accuracy model result with request file: ${accuData.reqJson} ").getBytes("utf-8"))
+      out.write(("=" * 10 + "\n").getBytes("utf-8"))
+
+      val ((missCount, srcCount), missedList) = accuData.result
+      val rslt = s"match percentage: ${((1 - missCount.toDouble / srcCount) * 100)} %"
+      val rcds = missedList.mkString("\n")
+      val rcd = rslt + "\n\n" + rcds + "\n\n";
+
+      out.write(rcd.getBytes("utf-8"))
+    }
+    out.close()
+    sc.stop()
   }
 }
--- bark-models/src/test/scala/modelTest/ValiTest.scala
+++ bark-models/src/test/scala/modelTest/ValiTest.scala
@@ -1,130 +1,78 @@
 package modelTest
 
 import org.apache.bark._
-import org.apache.bark.util._
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.StructType
 import org.apache.spark.{SparkConf, SparkContext}
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite, Matchers}
 
-import scala.collection.mutable.ArrayBuffer
-import org.scalatest.Assertions
-import org.scalatest.Matchers
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, Row, SQLContext}
-import java.io.{File, FileInputStream, FileOutputStream}
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import java.io.{FileInputStream, FileOutputStream}
+import scala.collection.mutable.MutableList
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-import scala.util.matching.Regex
+
 @RunWith(classOf[JUnitRunner])
-class ValiTest extends FunSuite with Matchers {
+class ValiTest extends FunSuite with Matchers with BeforeAndAfter {
 
-  val dataFilePath = convertPath("data/test/dataFile/")
-  val reqJsonPath = convertPath("data/test/reqJson/")
-  val recordFilePath = convertPath("data/test/recordFile/")
+  val dataFilePath = FileLoaderUtil.convertPath("data/test/dataFile/")
+  val reqJsonPath = FileLoaderUtil.convertPath("data/test/reqJson/")
+  val recordFilePath = FileLoaderUtil.convertPath("data/test/recordFile/")
   val recordFileName = "_RESULT_VALI"
-  val dataDescFileSuffix = "_type"
 
-  def convertPath(path: String) : String = {
-    path.replace("/", System.getProperty("file.separator"))
+  case class ValiData() {
+    var cnt: Int = _
+    var reqJson: String = _
+    var configure: ValidityConfEntity = _
+    var dataFrame: DataFrame = _
   }
+  val valiDatas = MutableList[ValiData]()
 
-  def getValiTestReq() : ArrayBuffer[String] = {
-    val jsonFiles = ArrayBuffer[String](reqJsonPath + "valiTest1.json", reqJsonPath + "valiTest2.json")
-    jsonFiles
-  }
+  var sc: SparkContext = _
 
-  def loadDataFile(sqlContext: SQLContext, file: String, fmt: String = "com.databricks.spark.csv") = {
-    val reader = sqlContext.read
-      .format(fmt)
-      .option("header", "true")
-      .option("treatEmptyValuesAsNulls", "true")
-      .option("delimiter", ",")
-
-    val typeFile = file + dataDescFileSuffix
-    val typeExist = new File(typeFile).exists()
-    val schemaReader = if (typeExist) {
-      val types = reader.load(typeFile).collect.map( r => (r.getString(0), r.getString(1)) )
-      val fields = types.map(kt => StructField(kt._1, DataTypeUtils.str2DataType(kt._2)))
-      val customSchema = StructType(fields)
-      reader.schema(customSchema)
-    } else {
-      reader.option("inferSchema", "true")
-    }
-
-    schemaReader.load(file)
-  }
-
-  test("test validity file with srcFile") {
-    val reqFiles = getValiTestReq()
-
-    val conf = new SparkConf().setMaster("local[*]").setAppName("ValiTest")
-    val sc = new SparkContext(conf)
+  before {
+    val conf = new SparkConf().setMaster("local[*]").setAppName("AccTest")
+    sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
-
     val mapper = new ObjectMapper()
     mapper.registerModule(DefaultScalaModule)
 
     var cnt = 1;
-
-    val out = new FileOutputStream(recordFilePath + recordFileName)
-
-    reqFiles.foreach { reqFile =>
-      val input = new FileInputStream(reqFile)
-      val configure = mapper.readValue(input, classOf[ValidityConfEntity])
-
-      //source DataFrame
-      val srcFile = dataFilePath + configure.dataSet
-      val srcdf = loadDataFile(sqlContext, srcFile)
-
-      //-- algorithm --
-      Vali.calcVali(sc, configure, srcdf)
-
-      //output
-      out.write(("//" + "=" * 10).getBytes("utf-8"))
-      out.write((s" $cnt. Test Validity model result with request file: $reqFile ").getBytes("utf-8"))
-      out.write(("=" * 10 + "\n").getBytes("utf-8"))
-
-      val rcd = configure.toString + "\n\n"
-      out.write(rcd.getBytes("utf-8"))
-
+    val valiTests = List("valiTest1.json", "valiTest2.json")
+    for (tf <- valiTests) {
+      val reqJson = reqJsonPath + tf
+      val valiData = new ValiData()
+      valiData.cnt = cnt
+      valiData.reqJson = reqJson
+      val input = new FileInputStream(reqJson)
+      valiData.configure = mapper.readValue(input, classOf[ValidityConfEntity])
+      valiData.dataFrame = FileLoaderUtil.loadDataFile(sqlContext, dataFilePath + valiData.configure.dataSet)
+      valiDatas += valiData
       cnt += 1
     }
-
-    out.close()
-
-    sc.stop()
   }
 
-//  test("load file") {
-//    val conf = new SparkConf().setMaster("local[*]").setAppName("loadFile")
-//    val sc = new SparkContext(conf)
-//    if (sc.version.charAt(0) - '0' < 2) {
-//      val sqlContext = new SQLContext(sc)
-//
-//      val file = "data/test/dataFile/testFile"
-//      loadDataFile(sqlContext, file)
-//    }
-//  }
+  test("test validity requests") {
+    for (valiData <- valiDatas) {
+      //-- algorithm --
+      Vali.calcVali(valiData.configure, valiData.dataFrame)
+    }
+  }
 
-//  test("test clause") {
-//    import org.apache.bark.common.PartitionPair
-//    val partition1 = List[PartitionPair]()
-//    val partition2 = List[PartitionPair](new PartitionPair("name", "vincent"))
-//    val partition3 = List[PartitionPair](new PartitionPair("name", "vincent"), new PartitionPair("name", "lionel"), new PartitionPair("name", "shawn"))
-//    val partitions1 = List[List[PartitionPair]]()
-//    val partitions2 = List[List[PartitionPair]](partition1)
-//    val partitions3 = List[List[PartitionPair]](partition2, partition3)
-//
-//    println(PartitionUtils.generateSourceSQLClause("table1", partition1))
-//    println(PartitionUtils.generateSourceSQLClause("table1", partition2))
-//    println(PartitionUtils.generateSourceSQLClause("table1", partition3))
-//    println(PartitionUtils.generateTargetSQLClause("table2", partitions1))
-//    println(PartitionUtils.generateTargetSQLClause("table2", partitions2))
-//    println(PartitionUtils.generateTargetSQLClause("table2", partitions3))
-//  }
+  after {
+    val out = new FileOutputStream(recordFilePath + recordFileName)
+    for (valiData <- valiDatas) {
+      //output
+      out.write(("//" + "=" * 10).getBytes("utf-8"))
+      out.write((s" ${valiData.cnt}. Test Validity model result with request file: ${valiData.reqJson} ").getBytes("utf-8"))
+      out.write(("=" * 10 + "\n").getBytes("utf-8"))
+
+      val rcd = valiData.configure.toString + "\n\n"
+      out.write(rcd.getBytes("utf-8"))
+    }
+    out.close()
+    sc.stop()
+  }
 }
