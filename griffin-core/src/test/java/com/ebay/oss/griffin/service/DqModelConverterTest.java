32476fbbb252d8b8925a847300d74caaccbc90a8
lzhixing <lzhixing@ebay.com>
2016-09-18 14:02:39 +0800

    fix merge bugs

--- griffin-core/src/test/java/com/ebay/oss/griffin/service/DqModelConverterTest.java
+++ griffin-core/src/test/java/com/ebay/oss/griffin/service/DqModelConverterTest.java
@@ -1,26 +1,27 @@
 package com.ebay.oss.griffin.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import com.ebay.oss.bark.domain.DqModel;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.ebay.oss.griffin.service.DqModelConverter;
+import com.ebay.oss.griffin.domain.DqModel;
 
 
 public class DqModelConverterTest {
 
-    private DqModelConverter converter;
+	private DqModelConverter converter;
 
-    @Before
-    public void setUp() {
-        converter = new DqModelConverter();
-    }
-    
-    @Test
-    public void testVoOf() {
-        assertNull(converter.voOf(null));
-        assertNotNull(converter.voOf(new DqModel()));
-    }
+	@Before
+	public void setUp() {
+		converter = new DqModelConverter();
+	}
+
+
+	@Test
+	public void testVoOf() {
+		assertNull(converter.voOf(null));
+		assertNotNull(converter.voOf(new DqModel()));
+	}
 }
